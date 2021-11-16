9b36752e3bf4ac779995e8b750058cc24c5f7649
Zaven <zaven.avanesyan93@gmail.com>
2021-11-16 11:33:13 +0400

    n

--- src/index.js
+++ src/index.js
@@ -13,6 +13,14 @@ window.addEventListener("DOMContentLoaded", (event) => {
   );
 });
 
+window.renderApp = () =>
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById("building-app")
+  );
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
